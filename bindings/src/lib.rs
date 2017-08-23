9a1609afe694bdae1c926e05b6da21d11f54060e
Andreas C. Osowski <andreas@osowski.de>
2017-08-24 00:09:37 +0200

    bindings: simplify

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,14 +1,8 @@
-#![feature(start)]
 #![feature(alloc)]
-#![feature(lang_items)]
-#![feature(link_args)]
 #![no_std]
 
-#![cfg(not(test))]
-#![feature(core_intrinsics)]
-#![crate_type = "staticlib"]
-
 extern crate alloc;
+
 extern crate cty;
 
 extern crate iota_mam;
@@ -17,19 +11,6 @@ extern crate iota_trytes;
 extern crate iota_curl_cpu;
 extern crate iota_curl;
 extern crate iota_sign;
-
-#[cfg(not(test))]
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
-
-#[cfg(not(test))]
-#[lang = "panic_fmt"]
-fn panic_fmt() -> ! {
-    use core::intrinsics;
-    unsafe {
-        intrinsics::abort();
-    }
-}
 
 mod util;
 pub mod mam;
