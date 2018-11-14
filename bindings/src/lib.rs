c7c6d48926518061ee747d766055bc6c2c78e664
Andreas C. Osowski <andreas@osowski.de>
2018-11-14 15:21:21 +0100

    bindings: improve parse return value

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -7,11 +7,11 @@ extern crate alloc;
 extern crate cty;
 
 extern crate iota_bindings_shared as shared;
+extern crate iota_curl;
+extern crate iota_curl_cpu;
 extern crate iota_mam;
 extern crate iota_merkle;
-extern crate iota_trytes;
-extern crate iota_curl_cpu;
-extern crate iota_curl;
 extern crate iota_sign;
+extern crate iota_trytes;
 
 pub mod mam;
--- mam/src/errors.rs
+++ mam/src/errors.rs
@@ -1,13 +1,13 @@
 use alloc::string::String;
 
 #[derive(Eq, PartialEq, Debug)]
+#[repr(C)]
 pub enum MamError {
+    None,
     /// Message Hash did not have any hamming weight of zero
     InvalidHash,
     /// Signature did not match expected root
     InvalidSignature,
     /// Array was too short
     ArrayOutOfBounds,
-    /// Custom error
-    CustomError(String),
 }
